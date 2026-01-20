dac5db8ba587b5344ec2bbfe1ffbe49f25b57eb2
marzieh.dolati@dayasoft.com <marzieh.dolati@dayasoft.com>
2026-01-20 14:51:46 +0100

    fix: 🐛 update missed select event type component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-  
-  <Loading
-    v-if="events.length === 0"
-    text="Loading events..."
-  />
+  <div class="mb-4 flex flex-wrap items-center gap-3">
+    <h1 class="text-xl font-semibold">Events</h1>
 
-  <section
-    v-else
-    class="flex flex-wrap gap-4 mx-auto p-4"
-  >
+    <div class="ml-auto">
+      <EventTypeFilter v-model="selectedType" class="mb-4 mx-auto" />
+    </div>
+  </div>
+  <Loading v-if="events.length === 0" text="Loading events..." />
+
+  <section v-else class="flex flex-wrap gap-4 mx-auto p-4">
     <div
-      v-for="event in events"
+      v-for="event in filteredEvents"
       :key="event.id"
       class="min-w-[300px] max-w-[300px]"
     >
@@ -30,16 +30,22 @@
   </section>
 </template>
 <script setup lang="ts">
-import type { ApiEvent } from "~/types";
+import type { ApiEvent, EventType } from "~/types";
 import { useLocalEventsStore } from "@/stores/localEvents";
 
 const showDeleteModal = ref(false);
 const selectedEvent = ref<ApiEvent | null>(null);
 const store = useLocalEventsStore();
 const events = computed(() => store.events);
+const selectedType = ref<"all" | EventType>("all");
 
 function onDeleteClick(event: ApiEvent) {
   selectedEvent.value = event;
   showDeleteModal.value = true;
 }
+
+const filteredEvents = computed(() => {
+  if (selectedType.value === "all") return events.value;
+  return events.value.filter((e) => e.type === selectedType.value);
+});
 </script>
